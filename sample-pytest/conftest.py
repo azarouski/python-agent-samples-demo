3a42bed11e1d3e56c469ba01f9eebd510f39cd44
azarouski <azarouski@solvd.com>
2023-04-04 12:12:52 +0300

    changes for demo

--- sample-pytest/conftest.py
+++ sample-pytest/conftest.py
@@ -8,17 +8,22 @@ from selenium import webdriver
 def driver():
     zebrunner_selenium_grid = os.getenv("ZEBRUNNER_SELENIUM_GRID", "http://127.0.0.1:4444")
 
-    ############################################################################################
-    options = webdriver.ChromeOptions()
-    options.platform_name = "linux"
-    options.browser_version = "109.0"
-    options.set_capability("enableVideo", "true")
-    ############################################################################################
+    if zebrunner_selenium_grid == "http://127.0.0.1:4444" :
+        ############################################################################################
+        options = webdriver.ChromeOptions()
+        options.platform_name = "linux"
+        options.browser_version = "109.0"
+        options.set_capability("enableVideo", "true")
+        ############################################################################################
 
-    driver = webdriver.Remote(
-        command_executor=zebrunner_selenium_grid,
-        options=options
-    )
+        driver = webdriver.Remote(
+            command_executor=zebrunner_selenium_grid,
+            options=options
+        )
+    else:
+        driver = webdriver.Remote(
+            command_executor=zebrunner_selenium_grid
+        )
 
     driver.implicitly_wait(10)
     driver.maximize_window()
